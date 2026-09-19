ac66a0ed01d12172c1911ff6723d60084771545d
Александр Радаев <sasha555990@gmail.com>
2026-09-19 17:32:13 +0300

    поправил роуты и привязал гварды

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router'
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component'
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component'
+import { guestGuard } from './core/auth/guest.guard'
+
+export const routes: Routes = [
+          {
+            path: 'login',
+            canActivate: [guestGuard],
+            loadComponent: () => import('./pages/login/page-login.component')
+          },
+          {
+            path: 'register',
+            canActivate: [guestGuard],
+            loadComponent: () =>
+              import('./pages/register/page-register.component')
+          }
+        ]
+      },
+      {
+        path: 'recipes',
+        loadComponent: () => import('./pages/recipes/page-recipes.component')
+      },
+      {
+        path: 'favorites',
+        canActivate: [authGuard],
+        loadComponent: () => import('./pages/recipe/recipe.component')
+      },
+]
