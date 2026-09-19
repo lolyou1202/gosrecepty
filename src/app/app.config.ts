1b07b0a398012e245245af21e295f9c97209d7d5
Александр Радаев <sasha555990@gmail.com>
2026-09-19 17:22:43 +0300

    еще одна корректировка файлов

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,32 +1,27 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  provideZoneChangeDetection
-} from '@angular/core'
-import { provideRouter } from '@angular/router'
-import { routes } from './app-routing.module'
-import { registerLocaleData } from '@angular/common'
-import localeRu from '@angular/common/locales/ru'
-
-registerLocaleData(localeRu)
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import {
   provideRouter,
   withComponentInputBinding,
   withRouterConfig
 } from '@angular/router'
+import { registerLocaleData } from '@angular/common'
 import { routes } from './app.routes'
 import { authInterceptor } from './core/auth/auth.interceptor'
+import localeRu from '@angular/common/locales/ru'
 
+registerLocaleData(localeRu)
+
+export const appConfig: ApplicationConfig = {
+  providers: [
     provideHttpClient(withInterceptors([authInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
     provideRouter(
       routes,
       withRouterConfig({
         paramsInheritanceStrategy: 'always'
       }),
       withComponentInputBinding()
-    ),
+    )
   ]
 }
