42d806ec2c735001364f8cfc5e90544de521a902
Александр Радаев <sasha555990@gmail.com>
2026-09-21 14:17:29 +0300

    Release 0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,40 @@
 import { Routes } from '@angular/router'
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component'
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component'
 import { guestGuard } from './core/auth/guest.guard'
 import { authGuard } from './core/auth/auth.guard'
 
 export const routes: Routes = [
   {
-    path: 'login',
-    canActivate: [guestGuard],
-    loadComponent: () => import('./pages/login/page-login.component')
-  },
-  {
-    path: 'register',
-    canActivate: [guestGuard],
-    loadComponent: () => import('./pages/register/page-register.component')
-  },
-  {
-    path: 'recipes',
-    loadComponent: () => import('./pages/recipes/page-recipes.component')
-  },
-  {
-    path: 'favorites',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/recipe/recipe.component')
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+          {
+            path: 'login',
+            canActivate: [guestGuard],
+            loadComponent: () => import('./pages/login/page-login.component')
+          },
+          {
+            path: 'register',
+            canActivate: [guestGuard],
+            loadComponent: () =>
+              import('./pages/register/page-register.component')
+          }
+        ]
+      },
+      {
+        path: 'recipes',
+        loadComponent: () => import('./pages/recipes/page-recipes.component')
+      },
+      {
+        path: 'favorites',
+        canActivate: [authGuard],
+        loadComponent: () => import('./pages/recipe/recipe.component')
+      },
+    ]
   }
 ]
